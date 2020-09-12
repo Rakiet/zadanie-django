2577c0d8d6adaee37dabe0e16662915eafb9fa6b
rakiet <piter5331@gmail.com>
2020-09-12 16:02:01 +0200

    dodanie edycji i usuwania dynamicznego

--- kinoapp/urls.py
+++ kinoapp/urls.py
@@ -4,7 +4,7 @@ from kinoapp.views import wszystkie_filmy, nowy_film, edytuj_film, usun_film
 
 urlpatterns = [
     path('filmy/', wszystkie_filmy),
-    path('nowy/', nowy_film),
-    path('edytuj/<int:id>/', edytuj_film),
-    path('usun/<int:id>/', usun_film),
+    path('nowy/', nowy_film, name="nowy_film"),
+    path('edytuj/<int:id>/', edytuj_film, name="edytuj_film"),
+    path('usun/<int:id>/', usun_film, name="usun_film"),
 ]
