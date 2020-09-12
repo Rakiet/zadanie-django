0aa7ba7492530074a24578bc3dc3171eacd2dd67
rakiet <piter5331@gmail.com>
2020-09-12 14:45:20 +0200

    dodanie funcji nowy film

--- kinoapp/views.py
+++ kinoapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpRequest
+from .models import Kino
+from .forms import KinoForm
+
+def wszystkie_filmy(request):
+    wszyskie = Kino.objects.all()
+    return render(request, 'filmy.html', {'filmy': wszyskie})
+
+def nowy_film(request):
+    form = KinoForm(request.POST or None, request.FILES or None)
+
+    if form.is_valid():
+        form.save()
+    return render(request, 'nowy_film.html', {'form': form})
 
 # Create your views here.
