734f316d0b8c3a6e163407b074ed83a8b11b3383
rakiet <piter5331@gmail.com>
2020-09-12 16:26:53 +0200

    dodanie nazwy url

--- kinoapp/urls.py
+++ kinoapp/urls.py
@@ -3,7 +3,7 @@ from kinoapp.views import wszystkie_filmy, nowy_film, edytuj_film, usun_film
 
 
 urlpatterns = [
-    path('filmy/', wszystkie_filmy),
+    path('filmy/', wszystkie_filmy, name="wszystkie_filmy"),
     path('nowy/', nowy_film, name="nowy_film"),
     path('edytuj/<int:id>/', edytuj_film, name="edytuj_film"),
     path('usun/<int:id>/', usun_film, name="usun_film"),
