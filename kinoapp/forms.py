3886c7d1074877e71077c909e172b98759b5a1d8
rakiet <piter5331@gmail.com>
2020-09-13 15:51:07 +0200

    dodanie dodatkowe_info oraz recenzji

--- kinoapp/forms.py
+++ kinoapp/forms.py
@@ -6,13 +6,13 @@ class KinoForm(ModelForm):
         model = Kino
         fields = ['tytul', 'rok', 'opis', 'cena', 'plakat', 'ranking_imdb', 'data']
 
-class DodatkoweInfo(ModelForm):
+class DodatkoweInfoForm(ModelForm):
     class Meta:
         model = DodatkoweInfo
         fields = ['czas_trwania', 'gatunek']
 
 
-class Ocena(ModelForm):
+class OcenaForm(ModelForm):
     class Meta:
         model = Ocena
         fields = ['recenzja', 'gwiazdki']
--- kinoapp/views.py
+++ kinoapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpRequest
-from .models import Kino
-from .forms import KinoForm, DodatkoweInfo, Ocena
+from django.core.exceptions import ObjectDoesNotExist
+from .models import Kino, DodatkoweInfo, Ocena
+from .forms import KinoForm, DodatkoweInfoForm, OcenaForm
 from django.contrib.auth.decorators import login_required
 
 
@@ -13,26 +13,43 @@ def wszystkie_filmy(request):
 @login_required()
 def nowy_film(request):
     form_film = KinoForm(request.POST or None, request.FILES or None)
-    form_dodatkowy = DodatkoweInfo(request.POST or None)
-    form_ocena = Ocena(request.POST or None)
+    form_dodatkowe = DodatkoweInfoForm(request.POST or None)
 
-    if all((form_film.is_valid(), form_ocena.is_valid(), form_dodatkowy.is_valid())):
+    if all((form_film.is_valid(), form_dodatkowe.is_valid())):
         film = form_film.save(commit=False)
-        dodatkowe = form_dodatkowy.save()
+        dodatkowe = form_dodatkowe.save()
         film.dodatkowe = dodatkowe
         film.save()
         return redirect(wszystkie_filmy)
-    return render(request, 'film_form.html', {'form': form_film, 'form_dodatkowy': form_dodatkowy, 'form_ocena': form_ocena, 'nowy': True})
+    return render(request, 'film_form.html', {'form': form_film, 'form_dodatkowe': form_dodatkowe, 'nowy': True})
 
 @login_required()
 def edytuj_film(request, id):
     film = get_object_or_404(Kino, pk=id)
-    form = KinoForm(request.POST or None, request.FILES or None, instance=film)
+    oceny = Ocena.objects.filter(film=film)
 
-    if form.is_valid():
-        form.save()
+    try:
+        dodatkowe = DodatkoweInfo.objects.get(pk=film.id)
+    except DodatkoweInfo.DoesNotExist:
+        dodatkowe = None
+
+    form_film = KinoForm(request.POST or None, request.FILES or None, instance=film)
+    form_dodatkowe = DodatkoweInfoForm(request.POST or None, instance=dodatkowe)
+    form_ocena = OcenaForm(request.POST or None)
+
+    if request.method == 'POST':
+        if 'gwiazdki' in request.POST:
+            ocena = form_ocena.save(commit=False)
+            ocena.film = film
+            ocena.save()
+
+    if all((form_film.is_valid(), form_dodatkowe.is_valid())):
+        film = form_film.save(commit=False)
+        dodatkowe = form_dodatkowe.save()
+        film.dodatkowe = dodatkowe
+        film.save()
         return redirect(wszystkie_filmy)
-    return render(request, 'film_form.html', {'form': form, 'nowy': False})
+    return render(request, 'film_form.html', {'form': form_film, 'form_dodatkowe': form_dodatkowe, 'oceny': oceny, 'form_ocena': form_ocena, 'nowy': False,})
 
 @login_required()
 def usun_film(request, id):
