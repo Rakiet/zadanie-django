06f854b7ebe88fac227edb21c693cb0b9bf27651
rakiet <piter5331@gmail.com>
2020-09-13 11:40:34 +0200

    dodanie do forms class Dodatkowe info oraz ocena

--- kinoapp/forms.py
+++ kinoapp/forms.py
@@ -1,7 +1,18 @@
 from django.forms import ModelForm
-from .models import Kino
+from .models import Kino, DodatkoweInfo, Ocena
 
 class KinoForm(ModelForm):
     class Meta:
         model = Kino
         fields = ['tytul', 'rok', 'opis', 'cena', 'plakat', 'ranking_imdb', 'data']
+
+class DodatkoweInfo(ModelForm):
+    class Meta:
+        model = DodatkoweInfo
+        fields = ['czas_trwania', 'gatunek']
+
+
+class Ocena(ModelForm):
+    class Meta:
+        model = Ocena
+        fields = ['recenzja', 'gwiazdki']
