f74158520b8d2eb31ae5d1e12e127472bed48e4d
rakiet <piter5331@gmail.com>
2020-09-13 12:11:00 +0200

    dodanie dodatkowe_info

--- kinoapp/views.py
+++ kinoapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpRequest
 from .models import Kino
-from .forms import KinoForm
+from .forms import KinoForm, DodatkoweInfo, Ocena
 from django.contrib.auth.decorators import login_required
 
 
@@ -12,12 +12,17 @@ def wszystkie_filmy(request):
 
 @login_required()
 def nowy_film(request):
-    form = KinoForm(request.POST or None, request.FILES or None)
+    form_film = KinoForm(request.POST or None, request.FILES or None)
+    form_dodatkowy = DodatkoweInfo(request.POST or None)
+    form_ocena = Ocena(request.POST or None)
 
-    if form.is_valid():
-        form.save()
+    if all((form_film.is_valid(), form_ocena.is_valid(), form_dodatkowy.is_valid())):
+        film = form_film.save(commit=False)
+        dodatkowe = form_dodatkowy.save()
+        film.dodatkowe = dodatkowe
+        film.save()
         return redirect(wszystkie_filmy)
-    return render(request, 'film_form.html', {'form': form, 'nowy': True})
+    return render(request, 'film_form.html', {'form': form_film, 'form_dodatkowy': form_dodatkowy, 'form_ocena': form_ocena, 'nowy': True})
 
 @login_required()
 def edytuj_film(request, id):
